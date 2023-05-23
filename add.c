c2f0bf15a40185e52f6d4662b6fd2f8e06c24d99
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-23 22:09:40 +0300

    add

---
+++ add.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+
+/**
+ * op_add       - Adds the top two elements of @stack
+ *
+ * @stack:        A doubly linked list
+ * @line_number:  Current line number in monty script file
+ */
+
+void op_add(stack_t **stack, unsigned int line_number)
+{
+	stack_t *target;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+	{
+		printf("L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	target = *stack;
+	(*stack)->next->n += (*stack)->n;
+	*stack = (*stack)->next;
+
+	free(target);
+}
