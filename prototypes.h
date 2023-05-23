02613c852abee84a6837a398044ea7a1f175dcea
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-23 21:04:27 +0300

    prototypes

--- prototypes.h
+++ prototypes.h
@@ -1,6 +1,17 @@
 #ifndef _PROTOTYPES_H_
 #define _PROTOTYPES_H_
 
-void push(stack_t **stack, unsigned int line_number);
+/* Stack manipulation functions */
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
+
+/* Stack display */
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+
+/* Node interaction */
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
 
 #endif /* _PROTOTYPES_H_ */
