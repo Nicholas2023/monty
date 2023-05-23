9ee9a15dba5e09da69a20c2f941d2e7566d85e6e
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-23 21:00:47 +0300

    monty.h

--- monty.h
+++ monty.h
@@ -1,6 +1,13 @@
-#ifndef MONTY_H_
-#define MONTY_H_
+#ifndef MONTY_H
+#define MONTY_H
 #include "prototypes.h"
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
