74137fa3c57f2a0617be7bbb4cce987e7b991e0e
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-23 23:39:31 +0300

    main

--- main_monty.c
+++ main_monty.c
@@ -1,108 +1,114 @@
 #include "monty.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-global_t global;
+char stack_queue = 's';
 
 /**
- * free_stack - Free memory used by stack
- */
-
-void free_stack(void)
-{
-	if (global.head != NULL)
-	{
-		while (global.head->next != NULL)
-		{
-			global.head = global.head->next;
-			free(global.head->prev);
-		}
-		free(global.head);
-	}
-}
-
-
-/**
- * find_function - Find function to match argument, if any
- * @stack: Head of stack data structure
- * @line_number: Line number of current line in monty file
+ * main - Entry point
+ * @argc: number of arguments passed as parameter to main program.
+ * @argv: array of strings with the parameters passed to main program
  *
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on any failure.
  */
-
-void find_function(stack_t **stack, unsigned int line_number)
+int main(int argc, char *argv[])
 {
-	unsigned int i = 0;
-	instruction_t functions[] = {
-		{"push", op_push}, {"pall", op_pall},
-		{"pint", op_pint}, {"pop", op_pop},
-		{"swap", op_swap}, {"add", op_add},
-		{"nop", op_nop}, {NULL, NULL}
-	};
-
-	while (functions[i].opcode != NULL)
-	{
-		if (strcmp(global.op_name, functions[i].opcode) == 0)
-		{
-			functions[i].f(stack, line_number);
-			break;
-		}
-		i++;
-	}
-
-	/* Could not find command in array, invalid command */
-	if (functions[i].opcode == NULL)
-	{
-		printf("L%u: unknown instruction %s\n", line_number, global.op_name);
-		exit(EXIT_FAILURE);
-	}
-}
-
-
-/**
- * main - Interprets Monty bytecodes
- * @argc: Number of arguments
- * @argv: Array of arguments
- *
- * Return: Success: EXIT_SUCCESS
- *         Failure: EXIT_FAILURE
- */
-
-int main(int argc, char **argv)
-{
-	stack_t *head = NULL;
-	FILE *fp;
-	char *line = NULL;
-	size_t line_length = 0;
+	FILE *stream = NULL;
+	size_t len = 0;
 	unsigned int line_number = 1;
+	char *line = NULL;
+	char *code = NULL;
+	stack_t *stack = NULL;
 
-	if (argc < 2)
+	if (argc != 2)
 	{
-		printf("USAGE: monty file\n");
+		dprintf(STDERR_FILENO, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
-	fp = fopen(argv[1], "r");
-	if (fp == NULL)
+	stream = fopen(argv[1], "r");
+	if (stream == NULL)
 	{
-		printf("Error: Can't open file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	atexit(free_stack);
 
-	while (fgets(line, line_length, fp) != NULL)
+	while (getline(&line, &len, stream) != -1)
 	{
-		global.op_name = strtok(line, "\n\t\r\v\f ");
-		global.value = strtok(NULL, "\n\t\r\v\f ");
-
-		if (global.op_name != NULL)
-		{
-			find_function(&head, line_number);
-		}
+		code = strtok(line, " \t\r\n\v\f");
+		if (code != NULL && code[0] != '#')
+			get_opcode(&stack, line_number, code);
 		line_number++;
 	}
 
+	free_stack_t(stack);
+	stack = NULL;
 	free(line);
-	fclose(fp);
-
+	fclose(stream);
 	exit(EXIT_SUCCESS);
+}
+
+/**
+ * get_opcode - reads opcode and verifies if is valid.
+ * @stack: double pointer to header (top) of the stack.
+ * @line_number: counter for line number of the file.
+ * @code: opcode to excecute.
+ *
+ * Return: void.
+ */
+void get_opcode(stack_t **stack, unsigned int line_number, char *code)
+{
+	int i = 0;
+	instruction_t opcode_func[] = {
+		{"add", _add},
+		{"div", _div},
+		{"mod", _mod},
+		{"mul", _mul},
+		{"nop", _nop},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"push", _push},
+		{"sub", _sub},
+		{"swap", _swap},
+		{"pchar", _pchar},
+		{"pstr", _pstr},
+		{"rotr", _rotr},
+		{"rotl", _rotl},
+		{"stack", _stack},
+		{"queue", _queue},
+		{NULL, NULL}
+	};
+
+	while (opcode_func[i].opcode)
+	{
+		if (strcmp(opcode_func[i].opcode, code) == 0)
+		{
+			opcode_func[i].f(stack, line_number);
+			return;
+		}
+		i++;
+	}
+	dprintf(STDERR_FILENO, "L%u: unknown instruction %s\n", line_number, code);
+	free_stack_t(*stack);
+	/**
+	 * close_file
+	 */
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * free_stack_t - function that free a list of type dlistint_t
+ * @head: pointer to a list type stack_t
+ *
+ * Return: void.
+ */
+void free_stack_t(stack_t *head)
+{
+	stack_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
