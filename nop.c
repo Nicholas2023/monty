97a07a361e3033780942694b38f3c2fb3124b35e
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-23 22:11:43 +0300

    nop

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+
+/**
+ * op_nop - Does nothing
+ * @stack: A doubly linked list
+ * @line_number: Current line number in monty script file
+ *
+ */
+
+void op_nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
