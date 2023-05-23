62900bf5bfb36763fb49d307ff269a6480bdcdab
Nicholas2023 <nicholasodhiambo2015@gmail.com>
2023-05-23 18:57:34 +0300

    monty.h

---
+++ monty.h
@@ -0,0 +1,38 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include "prototypes.h"
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: Integer
+ * @prev: Points to the previous element of the stack (or queue)
+ * @next: Points to the next element of the stack (or queue)
+ *
+ * Description: Doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+
+/**
+ * struct instruction_s - Opcode and its function
+ * @opcode: The opcode
+ * @f: Function to handle the opcode
+ *
+ * Description: Opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+#endif /* MONTY_H */
